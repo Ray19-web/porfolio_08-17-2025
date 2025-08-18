0ce77c0275b5035cebddc568d44a7c66e825c313
Raymund Gorres <gorres.raymund18@gmail.com>
2025-08-17 16:46:28 -1200

    back

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/porfolio_08-17-2025"
 })
 
 
